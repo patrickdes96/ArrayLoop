5315242ffb23a56b86b0d784cb803f9f94224de1
patrickdes96 <pd14qr@brocku.ca>
2017-03-13 10:27:15 -0400

    Beautiful Algorithm

---
+++ ArrayLoop.java
@@ -0,0 +1,8 @@
+public class ArrayLoop{
+ 	public static void main(String[] args){
+		int[] array = {1, 2, 3, 4, 5, 6, 7}; 
+		for (int i=0; i < array.length; i++){
+			System.out.println(array[i]);
+		} // End of looping algorithm
+	}
+}
